b7b2d480d3ad1f9c2850523b337962898f0e5a7e
Karol <58816240+karol71927@users.noreply.github.com>
2024-05-20 09:08:56 +0200

    updated test

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,7 +1,28 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Collection, Entity, ManyToOne, MikroORM, OneToMany, PrimaryKey, Property } from '@mikro-orm/mysql';
 
 @Entity()
-class User {
+class Recipe {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  title: string;
+
+  @Property({ onCreate: () => new Date() })
+  createdAt!: Date;
+
+  @OneToMany(() => Ingredient, ingredient => ingredient.recipe)
+  ingredients = new Collection<Ingredient>(this);
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+}
+
+@Entity()
+class Ingredient {
 
   @PrimaryKey()
   id!: number;
@@ -9,12 +30,20 @@ class User {
   @Property()
   name: string;
 
-  @Property({ unique: true })
-  email: string;
+  @Property()
+  quantity: number;
 
-  constructor(name: string, email: string) {
+  @Property({
+    onCreate: () => new Date(),
+  })
+  createdAt!: Date;
+
+  @ManyToOne(() => Recipe)
+  recipe!: Recipe;
+
+  constructor(name: string, quantity: number) {
     this.name = name;
-    this.email = email;
+    this.quantity = quantity;
   }
 
 }
@@ -23,29 +52,42 @@ let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
+    entities: [Recipe],
+    dbName: '5577',
+    port: 3308,
+    timezone: '+02:00',
+    debug: true,
   });
   await orm.schema.refreshDatabase();
 });
 
-afterAll(async () => {
-  await orm.close(true);
-});
+afterAll(() => orm.close(true));
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+test('5577', async () => {
+  const r = new Recipe('My first recipe');
+  r.ingredients.add(new Ingredient('Flour', 500));
+  r.ingredients.add(new Ingredient('Sugar', 200));
+  r.ingredients.add(new Ingredient('Milk', 200));
+  orm.em.persist(r);
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const recipe = await orm.em.findOneOrFail(
+    Recipe,
+    { title: 'My first recipe' },
+    { populate: ['ingredients'] },
+  );
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  const withPopulate = recipe.ingredients
+    .getItems()
+    .find(x => x.name === 'Flour');
+
+  console.log(withPopulate);
+
+  const ingredients = await orm.em.find(Ingredient, {
+    recipe: { id: recipe.id },
+  });
+
+  const withoutPopulate = ingredients.find(x => x.name === 'Flour');
+  console.log(withoutPopulate);
 });
