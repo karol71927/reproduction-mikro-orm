e8377016579bb07406b37758cdd70472dc24d82c
Karol Kawalec <karol.kawalec@signalos.io>
2024-05-20 12:20:52 +0200

    working reproduction

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,8 +1,15 @@
-import { Collection, Entity, ManyToOne, MikroORM, OneToMany, PrimaryKey, Property } from '@mikro-orm/mysql';
+import {
+  Collection,
+  Entity,
+  ManyToOne,
+  MikroORM,
+  OneToMany,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/mysql";
 
 @Entity()
-class Recipe {
-
+export class Recipe {
   @PrimaryKey()
   id!: number;
 
@@ -12,18 +19,16 @@ class Recipe {
   @Property({ onCreate: () => new Date() })
   createdAt!: Date;
 
-  @OneToMany(() => Ingredient, ingredient => ingredient.recipe)
+  @OneToMany(() => Ingredient, (ingredient) => ingredient.recipe)
   ingredients = new Collection<Ingredient>(this);
 
   constructor(title: string) {
     this.title = title;
   }
-
 }
 
 @Entity()
-class Ingredient {
-
+export class Ingredient {
   @PrimaryKey()
   id!: number;
 
@@ -45,49 +50,60 @@ class Ingredient {
     this.name = name;
     this.quantity = quantity;
   }
-
 }
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    entities: [Recipe],
-    dbName: '5577',
-    port: 3308,
-    timezone: '+02:00',
+    entities: [Recipe, Ingredient],
+    dbName: "test",
+    port: 3306,
+    host: "test-db",
+    user: "root",
+    password: "root",
+    timezone: "+02:00",
     debug: true,
+    allowGlobalContext: true,
   });
   await orm.schema.refreshDatabase();
 });
 
 afterAll(() => orm.close(true));
 
-test('5577', async () => {
-  const r = new Recipe('My first recipe');
-  r.ingredients.add(new Ingredient('Flour', 500));
-  r.ingredients.add(new Ingredient('Sugar', 200));
-  r.ingredients.add(new Ingredient('Milk', 200));
+test("5577", async () => {
+  const r = new Recipe("My first recipe");
+  r.ingredients.add(new Ingredient("Flour", 500));
+  r.ingredients.add(new Ingredient("Sugar", 200));
+  r.ingredients.add(new Ingredient("Milk", 200));
   orm.em.persist(r);
   await orm.em.flush();
   orm.em.clear();
 
   const recipe = await orm.em.findOneOrFail(
     Recipe,
-    { title: 'My first recipe' },
-    { populate: ['ingredients'] },
+    { title: "My first recipe" },
+    { populate: ["ingredients"], disableIdentityMap: true }
   );
 
   const withPopulate = recipe.ingredients
     .getItems()
-    .find(x => x.name === 'Flour');
+    .find((x) => x.name === "Flour");
 
   console.log(withPopulate);
 
-  const ingredients = await orm.em.find(Ingredient, {
-    recipe: { id: recipe.id },
-  });
+  const ingredients = await orm.em.find(
+    Ingredient,
+    {
+      recipe: { id: recipe.id },
+    },
+    { disableIdentityMap: true }
+  );
 
-  const withoutPopulate = ingredients.find(x => x.name === 'Flour');
+  const withoutPopulate = ingredients.find((x) => x.name === "Flour");
   console.log(withoutPopulate);
+
+  console.log(withPopulate?.createdAt, withoutPopulate?.createdAt);
+
+  expect(withPopulate?.createdAt).toEqual(withoutPopulate?.createdAt);
 });
